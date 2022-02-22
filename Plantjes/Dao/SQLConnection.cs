f8dd52dc235667b93c63f20e175f033e634da614
Justine-Vanwynsberghe <99887117+Justine-Vanwynsberghe@users.noreply.github.com>
2022-02-22 10:18:02 +0100

    Delete SQLConnection.cs
    Verwijderen aangezien dit in Visual Studio werd aangepast.

--- Plantjes/Dao/SQLConnection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Plantjes.Dao
-{
-    internal class SQLConnection
-    {
-        //plaats hier de connectiestring naar je databank
-        internal static string connectionstring = 
-
-    }
-}
