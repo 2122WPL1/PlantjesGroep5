af12a6779bb39047d42d5eed2fe6e86125db6d17
MathiasVandoorneVives <79833571+MathiasVandoorneVives@users.noreply.github.com>
2022-03-15 11:08:54 +0100

    Delete SQLConnection.cs

--- Plantjes/Dao/SQLConnection.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Plantjes.Dao
-{
-    internal class SQLConnection
-    {
-        //plaats hier de connectiestring naar je databank
-        internal static string connectionstring = @"Data Source=LAPTOP-JKBD9744\VIVES;Initial Catalog=planten;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-    }
-}
