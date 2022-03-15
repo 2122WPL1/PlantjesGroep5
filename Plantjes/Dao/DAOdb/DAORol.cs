b9c46c1b1fc3befe0b9c3e063da2a942757d4e2f
Trúc Võ Thanh <r0598792@student.vives.be>
2022-03-15 12:29:29 +0100

    DAORol schrijven

--- Plantjes/Dao/DAOdb/DAORol.cs
+++ Plantjes/Dao/DAOdb/DAORol.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plantjes.Models.Db;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace Plantjes.Dao.DAOdb
 {
-    class DAORol
+    public class DAORol
     {
+        private static readonly DAORol instance = new DAORol();
+
+        /*Niet noodzakelijk voor de singletonpattern waar wel voor de DAOLogic*/
+        private readonly plantenContext context;
+
+        //2. private contructor
+        private DAORol()
+        {
+            /*Niet noodzakelijk voor de singletonpattern waar wel voor de DAOLogic*/
+            this.context = new plantenContext();
+        }
+
+
+        public static DAORol Instance()
+        {
+            return instance;
+        }
+
+        public List<Rol> GetAllRol()
+        {
+            var updateRol = context.Rols.ToList();
+            return updateRol;
+        }
     }
 }
